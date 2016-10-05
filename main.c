34d589d860d9caacec4bddabebedf422f4b3ecb4
Daniel Higley <daniel.higley@gmail.com>
2016-10-05 13:51:30 -0400

    Project set up

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ * chutes and ladders
+ *
+ * Daniel Higley
+ * CSC 362-001
+ * Programming Assignment 3
+ * Due 10/10/2016
+ *
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  char board[100]="  mHk nH l B He Flq p H  hByHlho H B  jr HFB ir j H  F ku gd  H pjB mH x  BF i H  m oB HlHFBhoH BB ";
+}
