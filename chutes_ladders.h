3e45ed4ae554a7832acc4ea0e7e68603127a437d
Daniel Higley <daniel.higley@gmail.com>
2016-10-05 22:10:22 -0400

    Add header file and move output to output.c

---
+++ chutes_ladders.h
@@ -0,0 +1,16 @@
+/*
+ * chutes_ladders.h
+ * Chutes and Ladders
+ *
+ * Daniel Higley
+ * CSC 362-001
+ * Programming Assignment 3
+ * Due 10/19/2016
+ *
+ */
+
+// Library Include Statements
+#include <stdio.h>
+
+// Function Prototypes
+void output(FILE *, char *, char *, char *);
