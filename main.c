62921e6e0d4d30ab4d0ec2a0ce1d3ccdb48351ac
Daniel Higley <daniel.higley@gmail.com>
2016-10-06 07:30:22 -0400

    Advance player pointers through to the end of the game

--- main.c
+++ main.c
@@ -16,24 +16,29 @@ int main(void) {
 
   // variables for the board and player pointers
   char board[100]="  mHk nH l B He Flq p H  hByHlho H B  jr HFB ir j H  F ku gd  H pjB mH x  BF i H  m oB HlHFBhoH BB ";
-  char *p1 = board, *p2 = board;        /* pointer for players 1 and 2 */
+  char *p1, *p2;        /* pointer for players 1 and 2 */
+  p1 = board;           /* start players at the beginning of the board */
+  p2 = board;
 
-  // TEST move
-  p1 = move(p1, p2, board, 1);
-  p2 = move(p2, p1, board, 2);
+  // move players until one reaches the end of the board
+  while (p1 <= board + 100 || p2 <= board + 100) {
+    p1 = move(p1, p2, board, 1);
+    p2 = move(p2, p1, board, 2);
 
-  // TEST output
-  /* output(fp, board, p1, p2); */
+    // write move results to the log file
+    output(fp, board, p1, p2);
+  }
+  /* printf("%c\n", *p1); */
+  /* printf("%c\n", *p2); */
 
   fclose(fp);     /* close the file */
 }
 
 /* move: roll the dice and move the player pointer to its new position on the board */
 char *move(char *player, char *viewer, char *board, int player_id) {
+  // random die roll
   int roll;
-
   roll = rand() % 6 + 1;
-  printf("%d\n", roll);
 
   return player += roll;
 }
